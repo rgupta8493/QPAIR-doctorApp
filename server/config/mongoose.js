f34a44ed1b443a5a0f22f381897033afa90c8ac5
rgupta8493 <r.gupta8493@ufl.edu>
2016-12-19 03:00:49 -0500

    mongo server lab connected

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,14 +4,18 @@ var mongoose = require('mongoose');
 // require file-system so that we can load, read, require all of the model files
 var fs = require('fs');
 // connect to the database
-mongoose.connect('mongodb://localhost/docAppTodo');
-
+if (process.env.DEV_ENV) {
+  //connect to mongodb local server
+  mongoose.connect("mongodb://localhost/docAppTodo"); //allows us to perform CRUD operations locally
+}
+else {
+  mongoose.connect("mongodb://ronak:ronak@ds139448.mlab.com:39448/docapptodo"); //on mongolab server
+}
 // specify the path to all of the models
 var models_path = __dirname + '/../models';
-
 // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
-fs.readdirSync(models_path).forEach(function(file) {
-  if(file.indexOf('.js') > 0) {
+fs.readdirSync(models_path).forEach(function (file) {
+  if (file.indexOf('.js') > 0) {
     require(models_path + '/' + file);
   }
 })
